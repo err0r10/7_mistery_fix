97c62df76284a21b66f2ec5aee3bfda264b7e05b
err0r10 <dimanah10@gmail.com>
2017-12-27 04:52:44 -0800

    add if discriminant < 0

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,9 +3,9 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
-    if discriminant == 0:
-        return root1, None
+    if discriminant < 0:
+        return None, None
+    elif discriminant == 0:
+        return (-b - sqrt(discriminant)) / (2 * a), None
     else:
-        return root1, root2
+        return (-b - sqrt(discriminant)) / (2 * a), (-b + sqrt(discriminant)) / (2 * a)
