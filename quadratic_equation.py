37225386e662cf7e124e87a903916ee7a89628c7
err0r10 <dimanah10@gmail.com>
2017-12-27 07:04:44 -0800

    return value first sqrt second b

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -6,6 +6,8 @@ def get_roots(a, b, c):
     if discriminant < 0:
         return None, None
     elif discriminant == 0:
-        return (-b - sqrt(discriminant)) / 2 * a, None
+
+        return - (sqrt(discriminant) + b) / 2 * a, None
+
     else:
-        return (-b - sqrt(discriminant)) / 2 * a, (-b + sqrt(discriminant)) / 2 * a
+        return - (sqrt(discriminant) + b) / 2 * a, (sqrt(discriminant) - b) / 2 * a
