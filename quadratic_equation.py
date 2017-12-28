e9f39bf8024dded892c20267ab3904e25cb6974d
err0r10 <dimanah10@gmail.com>
2017-12-28 01:10:14 -0800

    refactoring function get_roots

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,13 +3,6 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-
-    if discriminant > 0:
-        r1 = (sqrt(discriminant) + b) / 2 * a * (-1)
-        r2 = (sqrt(discriminant) - b) / 2 * a
-        return r1, r2
-    elif discriminant == 0:
-        r1 = (sqrt(discriminant) + b) / 2 * a * (-1)
-        return r1, None
-    else:
-        return None, None
+    x1 = (sqrt(discriminant) + b) / 2 * a * (-1) if discriminant >= 0 else None
+    x2 = (sqrt(discriminant) - b) / 2 * a if discriminant > 0 else None
+    return x1, x2
