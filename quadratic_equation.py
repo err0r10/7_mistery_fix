c02511aef1684aaca066bf3812f9f5922376af42
err0r10 <dimanah10@gmail.com>
2017-12-28 09:15:36 +0300

    add doc for function get_roots

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,6 +2,10 @@ from math import sqrt
 
 
 def get_roots(a, b, c):
+    '''
+    Функция высчитывает дискриминант квадратного трехчлена.
+    Возвращает корни квадратного уравнения x1 и x2
+    '''
     discriminant = b ** 2 - 4 * a * c
     x1 = (sqrt(discriminant) + b) / 2 * a * (-1) if discriminant >= 0 else None
     x2 = (sqrt(discriminant) - b) / 2 * a if discriminant > 0 else None
