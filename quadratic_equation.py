17fc24d89ea4fbed87d638086708f384e6ac8561
err0r10 <dimanah10@gmail.com>
2017-12-29 12:24:50 +0300

    change name variable

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,6 +3,6 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    x1 = (sqrt(discriminant) + b) / 2 * a * (-1) if discriminant >= 0 else None
-    x2 = (sqrt(discriminant) - b) / 2 * a if discriminant > 0 else None
-    return x1, x2
+    radical1 = (sqrt(discriminant) + b) / 2 * a * (-1) if discriminant >= 0 else None
+    radical2 = (sqrt(discriminant) - b) / 2 * a if discriminant > 0 else None
+    return radical1, radical2
