169e82bb0b9dfc4ab8e0e6146398848edab122a2
err0r10 <dimanah10@gmail.com>
2018-01-05 15:27:53 +0300

    choise value roots

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,6 +3,6 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    radical1 = (sqrt(discriminant) + b) / 2 * a * (-1) if discriminant >= 0 else None
-    radical2 = (sqrt(discriminant) - b) / 2 * a if discriminant > 0 else None
-    return radical1, radical2
+    root1 = (sqrt(discriminant) + b) / 2 * a * (-1) if discriminant >= 0 else None
+    root2 = (sqrt(discriminant) - b) / 2 * a if discriminant > 0 else None
+    return root1, root2
